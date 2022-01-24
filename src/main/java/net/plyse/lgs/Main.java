8474963df87f6df7504446e89e2a5ab5fa8facc8
rapha <raphael@dichler.com>
2022-01-24 23:10:42 +0100

    Initial commit

---
+++ src/main/java/net/plyse/lgs/Main.java
@@ -0,0 +1,13 @@
+package net.plyse.lgs;
+
+/**
+ * @author Raphael Dichler on 24.01.2022.
+ * @project LogicGateSimulator
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
