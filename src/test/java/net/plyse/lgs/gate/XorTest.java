6adbb832a006efc4257450d47c98baf47d0baef8
rapha <raphael@dichler.com>
2022-01-25 18:53:09 +0100

    pom.xml setup

--- src/test/java/net/plyse/lgs/gate/XorTest.java
+++ src/test/java/net/plyse/lgs/gate/XorTest.java
@@ -1,38 +1,42 @@
 package net.plyse.lgs.gate;
 
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Raphael Dichler on 25.01.2022.
  * @project LogicGateSimulator
  */
-class XorTest {
+public class XorTest {
 
-//    private Xor createXor(int binaryPattern) {
-//        String binaryString = Integer.toBinaryString(binaryPattern);
-//        int len = binaryString.length();
-//        boolean[] inputs = new boolean[len];
-//        for (int i = 0; i < len; i++) {
-//            inputs[i] = binaryString.charAt(i) == '1';
-//        }
-//
-//        return new Xor(inputs, false);
-//    }
-//
-//    @Test
-//    void operate() {
-//        LogicGate xor2Inputs = createXor(0b10);
-//        xor2Inputs.operate();
-//        assertTrue(xor2Inputs.outputStatus);
-//
-//        LogicGate xor5InputsFalse = createXor(0b01010);
-//        xor5InputsFalse.operate();
-//        assertFalse(xor5InputsFalse.outputStatus);
-//
-//        LogicGate xor5InputsTrue = createXor(0b01000);
-//        xor5InputsTrue.operate();
-//        assertTrue(xor5InputsTrue.outputStatus);
-//    }
+    public XorTest() {
+    }
+
+    private Xor createXor(int binaryPattern) {
+        String binaryString = Integer.toBinaryString(binaryPattern);
+        int len = binaryString.length();
+        boolean[] inputs = new boolean[len];
+        for (int i = 0; i < len; i++) {
+            inputs[i] = binaryString.charAt(i) == '1';
+        }
+
+        return new Xor(inputs, false);
+    }
+
+    @Test
+    public void operate() {
+        LogicGate xor2Inputs = createXor(0b10);
+        xor2Inputs.operate();
+        assertTrue(xor2Inputs.outputStatus);
+
+        LogicGate xor5InputsFalse = createXor(0b01010);
+        xor5InputsFalse.operate();
+        assertFalse(xor5InputsFalse.outputStatus);
+
+        LogicGate xor5InputsTrue = createXor(0b01000);
+        xor5InputsTrue.operate();
+        assertTrue(xor5InputsTrue.outputStatus);
+    }
 }
